0a5a2496f9a14d13776d41b5040305b4b1040b66
emilk92 <emilkosic@tutamail.com>
2023-10-18 01:54:52 +0200

    cpp-prog.cpp: update output message
    Signed-off-by: emilk92 <emilkosic@tutamail.com>

--- cpp-prog.cpp
+++ cpp-prog.cpp
@@ -58,7 +58,7 @@ int main(int argc, char *argv[])
 
      printf("Incoming connection from %s port %d\n",
             inet_ntoa(cli_addr.sin_addr), ntohs(cli_addr.sin_port));
-     send(newsockfd, "Hello, world! 2\n", 13, 0);
+     send(newsockfd, "Hello, world! 44\n", 13, 0);
 
      bzero(buffer,256);
      }
